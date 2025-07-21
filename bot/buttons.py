5404603e1d28cbaf780be8439375aeae747077c0
korotas <korota-sasha@bk.ru>
2025-07-21 19:15:53 +0400

    add deprecation 2

--- bot/buttons.py
+++ bot/buttons.py
@@ -3,7 +3,7 @@ from telegram import ReplyKeyboardMarkup, KeyboardButton
 main_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton("Sneeze"), KeyboardButton("Yawn")],
-        [KeyboardButton("#1"), KeyboardButton("#2"), KeyboardButton("#1 & #2")]
+        [KeyboardButton("#1"), KeyboardButton("#2")]
     ],
     resize_keyboard=True,
     one_time_keyboard=False
